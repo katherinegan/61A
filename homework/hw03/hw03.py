726eb7d5367c77e3007f76b54f51118f1ec7040d
Katherine Gan <katherinegan01@gmail.com>
2020-02-20 09:51:23 -0800

    add hw03 and updated hog project

---
+++ homework/hw03/hw03.py
@@ -0,0 +1,222 @@
+from operator import sub, mul
+HW_SOURCE_FILE = 'hw03.py'
+
+#############
+# Questions #
+#############
+
+
+def num_sevens(x):
+    """Returns the number of times 7 appears as a digit of x.
+
+    >>> num_sevens(3)
+    0
+    >>> num_sevens(7)
+    1
+    >>> num_sevens(7777777)
+    7
+    >>> num_sevens(2637)
+    1
+    >>> num_sevens(76370)
+    2
+    >>> num_sevens(12345)
+    0
+    >>> from construct_check import check
+    >>> # ban all assignment statements
+    >>> check(HW_SOURCE_FILE, 'num_sevens',
+    ...       ['Assign', 'AugAssign'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if x == 0:
+        return 0
+    if x % 10 == 7:
+        return num_sevens(x // 10) + 1
+    else:
+        return num_sevens(x // 10)
+
+
+def pingpong(n):
+    """Return the nth element of the ping-pong sequence.
+
+    >>> pingpong(7)
+    7
+    >>> pingpong(8)
+    6
+    >>> pingpong(15)
+    1
+    >>> pingpong(21)
+    -1
+    >>> pingpong(22)
+    0
+    >>> pingpong(30)
+    6
+    >>> pingpong(68)
+    2
+    >>> pingpong(69)
+    1
+    >>> pingpong(70)
+    0
+    >>> pingpong(71)
+    1
+    >>> pingpong(72)
+    0
+    >>> pingpong(100)
+    2
+    >>> from construct_check import check
+    >>> # ban assignment statements
+    >>> check(HW_SOURCE_FILE, 'pingpong', ['Assign', 'AugAssign'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    def helper_function(index, value, positive):
+        if index == n:
+            return value
+        if (index % 7 == 0 and positive == True) or ('7' in str(index) and positive == True):
+            return helper_function(index + 1, value - 1, False)
+        elif (index % 7 == 0 and positive == False) or ('7' in str(index) and positive == False):
+            return helper_function(index + 1, value + 1, True)
+        else:
+            return helper_function(index + 1, value + 1 if positive == True else value - 1, positive)
+    return helper_function(1, 1, True)
+
+
+def count_change(total):
+    """Return the number of ways to make change for total.
+
+    >>> count_change(7)
+    6
+    >>> count_change(10)
+    14
+    >>> count_change(20)
+    60
+    >>> count_change(100)
+    9828
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'count_change', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    def power_of_2(total, a):
+        if a > total:
+            return power_of_2(total, a // 2)
+        elif a > total // 2 and a <= total:
+            return a
+        else:
+            return power_of_2(total, a * 2)
+
+    def helper(n, m):
+        if n == 0:
+            return 1
+        elif n < 0:
+            return 0
+        elif m == 0:
+            return 0
+        else:
+            return helper(n-m, m) + helper(n, m//2)
+    return helper(total, power_of_2(total, 1))
+
+
+def missing_digits(n):
+    """Given a number a that is in sorted, increasing order,
+    return the number of missing digits in n. A missing digit is
+    a number between the first and last digit of a that is not in n.
+    >>> missing_digits(1248) # 3, 5, 6, 7
+    4
+    >>> missing_digits(1122) # No missing numbers
+    0
+    >>> missing_digits(123456) # No missing numbers
+    0
+    >>> missing_digits(3558) # 4, 6, 7
+    3
+    >>> missing_digits(4) # No missing numbers between 4 and 4
+    0
+    >>> from construct_check import check
+    >>> # ban while or for loops
+    >>> check(HW_SOURCE_FILE, 'missing_digits', ['While', 'For'])
+    True
+    """
+    def helper(n, expected):
+        if n < 10:
+            if expected - n - 1 < 0:
+                return 0
+            return expected - n - 1
+        elif expected - (n % 10) > 0:
+            return expected - (n % 10) - 1 + helper(n // 10, n % 10)
+        elif (n % 10) - expected == 0:
+            return helper(n // 10, expected)
+
+    return helper(n, n % 10)
+
+
+###################
+# Extra Questions #
+###################
+
+def flatten(lst):
+    """Returns a flattened version of lst.
+
+    >>> flatten([1, 2, 3])     # normal list
+    [1, 2, 3]
+    >>> x = [1, [2, 3], 4]      # deep list
+    >>> flatten(x)
+    [1, 2, 3, 4]
+    >>> x # Ensure x is not mutated
+    [1, [2, 3], 4]
+    >>> x = [[1, [1, 1]], 1, [1, 1]] # deep list
+    >>> flatten(x)
+    [1, 1, 1, 1, 1, 1]
+    >>> x
+    [[1, [1, 1]], 1, [1, 1]]
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def print_move(origin, destination):
+    """Print instructions to move a disk."""
+    print("Move the top disk from rod", origin, "to rod", destination)
+
+
+def move_stack(n, start, end):
+    """Print the moves required to move n disks on the start pole to the end
+    pole without violating the rules of Towers of Hanoi.
+
+    n -- number of disks
+    start -- a pole position, either 1, 2, or 3
+    end -- a pole position, either 1, 2, or 3
+
+    There are exactly three poles, and start and end must be different. Assume
+    that the start pole has at least n disks of increasing size, and the end
+    pole is either empty or has a top disk larger than the top n start disks.
+
+    >>> move_stack(1, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    >>> move_stack(2, 1, 3)
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 3
+    >>> move_stack(3, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 3 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 1
+    Move the top disk from rod 2 to rod 3
+    Move the top disk from rod 1 to rod 3
+    """
+    assert 1 <= start <= 3 and 1 <= end <= 3 and start != end, "Bad start/end"
+    "*** YOUR CODE HERE ***"
+
+
+def make_anonymous_factorial():
+    """Return the value of an expression that computes factorial.
+
+    >>> make_anonymous_factorial()(5)
+    120
+    >>> from construct_check import check
+    >>> # ban any assignments or recursion
+    >>> check(HW_SOURCE_FILE, 'make_anonymous_factorial', ['Assign', 'AugAssign', 'FunctionDef', 'Recursion'])
+    True
+    """
+    return 'YOUR_EXPRESSION_HERE'

522468a5099e8d8d19ee236f24b06f5dae8f7b19
Katherine Gan <katherinegan01@gmail.com>
2020-03-15 03:27:08 -0700

    update files

---
+++ labs/lab06/lab06.py
@@ -0,0 +1,126 @@
+this_file = 'lab06.py'
+
+
+def make_adder_inc(n):
+    """
+    >>> adder1 = make_adder_inc(5)
+    >>> adder2 = make_adder_inc(6)
+    >>> adder1(2) 
+    7
+    >>> adder1(2) # 5 + 2 + 1
+    8
+    >>> adder1(10) # 5 + 10 + 2
+    17
+    >>> [adder1(x) for x in [1, 2, 3]]
+    [9, 11, 13]
+    >>> adder2(5)
+    11
+    """
+    "*** YOUR CODE HERE ***"
+    counter = -1
+
+    def adder(x):
+        nonlocal n, counter
+        counter += 1
+        return n + x + counter
+    return adder
+
+
+def make_fib():
+    """Returns a function that returns the next Fibonacci number
+    every time it is called.
+
+    >>> fib = make_fib()
+    >>> fib()
+    0
+    >>> fib()
+    1
+    >>> fib()
+    1
+    >>> fib()
+    2
+    >>> fib()
+    3
+    >>> fib2 = make_fib()
+    >>> fib() + sum([fib2() for _ in range(5)])
+    12
+    >>> from construct_check import check
+    >>> # Do not use lists in your implementation
+    >>> check(this_file, 'make_fib', ['List'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    prev_num, next_num = -1, -1
+
+    def next_fib():
+        nonlocal prev_num, next_num
+        if next_num == -1:
+            prev_num = -1
+            next_num = 0
+            return 0
+        if prev_num == -1:
+            prev_num = 0
+            next_num = 1
+            return 1
+        prev_num, next_num = next_num, prev_num + next_num
+        return next_num
+    return next_fib
+
+# Generators
+
+
+def naturals():
+    """A generator function that yields the infinite sequence of natural
+    numbers, starting at 1.
+
+    >>> m = naturals()
+    >>> type(m)
+    <class 'generator'>
+    >>> [next(m) for _ in range(10)]
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    """
+    i = 1
+    while True:
+        yield i
+        i += 1
+
+
+def scale(it, multiplier):
+    """Yield elements of the iterable it scaled by a number multiplier.
+
+    >>> m = scale([1, 5, 2], 5)
+    >>> type(m)
+    <class 'generator'>
+    >>> list(m)
+    [5, 25, 10]
+
+    >>> m = scale(naturals(), 2)
+    >>> [next(m) for _ in range(5)]
+    [2, 4, 6, 8, 10]
+    """
+    "*** YOUR CODE HERE ***"
+    return (elem * multiplier for elem in it)
+
+
+def hailstone(n):
+    """
+    >>> for num in hailstone(10):
+    ...     print(num)
+    ...
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    if n == 1:
+        yield 1
+    else:
+        yield n
+        if n % 2 == 0:
+            yield from hailstone(n // 2)
+        else:
+            yield from hailstone(n * 3 + 1)
